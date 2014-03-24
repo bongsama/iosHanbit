e3d9d117943c61004d7d9b5a4898b02dc2ebfc5d
bongsama <kim@bongkwan.com>
2014-03-24 22:21:22 +0900

    setup

---
+++ iphone/03LavelTimer/03LavelTimer/UIColor+Random.m
@@ -0,0 +1,13 @@
+//
+//  UIColor+Random.m
+//  03LavelTimer
+//
+//  Created by bongkwan on 2014. 3. 24..
+//  Copyright (c) 2014년 저녁반. All rights reserved.
+//
+
+#import "UIColor+Random.h"
+
+@implementation UIColor (Random)
+
+@end
