85a89375ac5e36a49efe129a8c28c6615f69e53b
ret6125 <easy@ijiui-iMac.local>
2014-03-25 20:02:12 +0900

    Initial Commit

---
+++ 140325/TimerEx/TimerEx/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TimerEx
+//
+//  Created by 이지 on 14. 3. 25..
+//  Copyright (c) 2014년 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
