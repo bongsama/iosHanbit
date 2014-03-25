a2da7dffcd833cd40fe5216bb5dc39ecbe8f062f
bongsama <kim@bongkwan.com>
2014-03-25 20:42:50 +0900

    autosave

--- iphone/TimerEx/TimerEx/ViewController.m
+++ iphone/TimerEx/TimerEx/ViewController.m
@@ -32,6 +32,7 @@
     if(myTimer != nil)
     {
         UIAlertView *dlg=[[UIAlertView alloc]initWithTitle:@"경고" message:@"Start 버튼을 이미 눌렀습니다." delegate:nil cancelButtonTitle:@"확인" otherButtonTitles:nil, nil];
+        [dlg show];
     }
     
     
