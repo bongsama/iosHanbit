66bbf08b2915e5c5d2831b2504c599fc38dae8f7
bongsama <kim@bongkwan.com>
2014-03-25 21:57:25 +0900

    autosave

--- iphone/AlertSheetEx/AlertSheetEx/ViewController.m
+++ iphone/AlertSheetEx/AlertSheetEx/ViewController.m
@@ -42,7 +42,7 @@
 -(void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
 {
     if (alertView==alert1) {
-        _label1.text=@"첫번째에서 발생!! button";
+        _label1.text=@"첫번째에서 발생!! buttonIndex:%d",[buttonIndex ];
     }else if(alertView == alert2)
     {
         _label2.text=@"두번째에서 발생~~";
