70e9dc77b9b5f3f25f8ab332a483d2391e093383
bongsama <kim@bongkwan.com>
2014-03-26 21:34:40 +0900

    autosave

---
+++ iphone/EmptyNaviEx1/EmptyNaviEx1/SecondViewController.m
@@ -0,0 +1,51 @@
+//
+//  SecondViewController.m
+//  EmptyNaviEx1
+//
+//  Created by 이지 on 14. 3. 26..
+//  Copyright (c) 2014년 이지. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    [_label setText:@"second View"];
+    [_label setFont:[UIFont boldSystemFontOfSize:25]];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
