8eb8d2d65a05263a28c6c6a4b79ab532431c6d8f
bongsama <kim@bongkwan.com>
2014-03-26 20:56:06 +0900

    autosave

---
+++ iphone/emptyNaviEx1/emptyNaviEx1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  emptyNaviEx1
+//
+//  Created by bongkwan on 2014. 3. 26..
+//  Copyright (c) 2014년 저녁반. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ iphone/emptyNaviEx1/emptyNaviEx1Tests/emptyNaviEx1Tests.m
@@ -0,0 +1,34 @@
+//
+//  emptyNaviEx1Tests.m
+//  emptyNaviEx1Tests
+//
+//  Created by bongkwan on 2014. 3. 26..
+//  Copyright (c) 2014년 저녁반. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface emptyNaviEx1Tests : XCTestCase
+
+@end
+
+@implementation emptyNaviEx1Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
