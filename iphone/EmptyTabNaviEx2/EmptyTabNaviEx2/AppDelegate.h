e18068773b29085f71c72e5a7a1267b7ce1b304f
bongsama <kim@bongkwan.com>
2014-03-26 21:54:37 +0900

    autosave

---
+++ iphone/EmptyTabNaviEx2/EmptyTabNaviEx2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  EmptyTabNaviEx2
+//
+//  Created by bongkwan on 2014. 3. 26..
+//  Copyright (c) 2014년 저녁반. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
