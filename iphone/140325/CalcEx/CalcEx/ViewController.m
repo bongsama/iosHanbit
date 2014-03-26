d05ca0cd5597d4712e239d4f253493aa9fa52cf3
ret6125 <easy@ijiui-iMac.local>
2014-03-26 20:12:55 +0900

    autosave

--- iphone/140325/CalcEx/CalcEx/ViewController.m
+++ iphone/140325/CalcEx/CalcEx/ViewController.m
@@ -62,6 +62,19 @@
 }
 
 - (IBAction)btnDot:(UIButton *)sender {
+    //   점버튼
+    NSString*   title = [sender titleForState:UIControlStateNormal];
+    //출력창값
+    NSMutableString *mout = [NSMutableString stringWithString:_tfOut.text];
+    //출력창값이 0이거나 bFirst가  YES면 새로 찍고, 아니면 추가
+    if ([mout isEqualToString:@"0"]|| bDot){
+        [mout setString:title];
+        bDot = NO;
+    }else{
+        [mout appendString:title];
+    }
+    [_tfOut setText:mout];
+    
 }
 
 - (IBAction)btnResult:(UIButton *)sender {
