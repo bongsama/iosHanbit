194a2265892fd5836e6a6b3ff255dd62cba89c36
ret6125 <easy@ijiui-iMac.local>
2014-03-26 20:55:22 +0900

    Initial Commit

---
+++ iphone/140326/EmptyNaviEx1/EmptyNaviEx1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EmptyNaviEx1
+//
+//  Created by 이지 on 14. 3. 26..
+//  Copyright (c) 2014년 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
