7883ac6701a27f249c8147a63cb721b22c1c5297
ret6125 <easy@ijiui-iMac.local>
2014-03-26 20:02:01 +0900

    autosave

--- iphone/140325/CalcEx/CalcEx/ViewController.m
+++ iphone/140325/CalcEx/CalcEx/ViewController.m
@@ -29,9 +29,15 @@
 
 - (IBAction)btnNumeric:(UIButton *)sender {
 //    버튼숫자
-    NSString*   title = [sender titleColorForState:UIControlStateNormal];
+    NSString*   title = [sender titleForState:UIControlStateNormal];
     //출력창값
     NSMutableString *mout = [NSMutableString stringWithString:_tfOut.text];
+    //출력창값이 0이거나 bFirst가  YES면 새로 찍고, 아니면 추가
+    if ([mout isEqualToString:@"0"]|| bFirst){
+        [mout setString:title];
+    }else{
+        [mout appendString:title];
+    }
 }
 
 - (IBAction)btnOperator:(UIButton *)sender {
