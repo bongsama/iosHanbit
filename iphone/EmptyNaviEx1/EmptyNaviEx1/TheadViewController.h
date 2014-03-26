3abb744c0db868d0cad5da82e965920ef04cad17
bongsama <kim@bongkwan.com>
2014-03-26 21:40:26 +0900

    autosave

---
+++ iphone/EmptyNaviEx1/EmptyNaviEx1/TheadViewController.h
@@ -0,0 +1,13 @@
+//
+//  TheadViewController.h
+//  EmptyNaviEx1
+//
+//  Created by bongkwan on 2014. 3. 26..
+//  Copyright (c) 2014년 bongkwan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TheadViewController : UIViewController
+
+@end
---
+++ iphone/EmptyNaviEx1/EmptyNaviEx1/TheadViewController.m
@@ -0,0 +1,13 @@
+//
+//  TheadViewController.m
+//  EmptyNaviEx1
+//
+//  Created by bongkwan on 2014. 3. 26..
+//  Copyright (c) 2014년 bongkwan. All rights reserved.
+//
+
+#import "TheadViewController.h"
+
+@implementation TheadViewController
+
+@end
