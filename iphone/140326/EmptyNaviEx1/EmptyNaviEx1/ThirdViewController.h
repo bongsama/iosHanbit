c06caf5714b6a5080c0efbc00d718be22918cd91
ret6125 <easy@ijiui-iMac.local>
2014-03-26 21:40:22 +0900

    autosave

---
+++ iphone/140326/EmptyNaviEx1/EmptyNaviEx1/ThirdViewController.h
@@ -0,0 +1,13 @@
+//
+//  ThirdViewController.h
+//  EmptyNaviEx1
+//
+//  Created by 이지 on 14. 3. 26..
+//  Copyright (c) 2014년 이지. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThirdViewController : UIViewController
+
+@end
