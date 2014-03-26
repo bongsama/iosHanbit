7ab30c9267c7484ccda3c8f13c54d96c81a5d5b7
ret6125 <easy@ijiui-iMac.local>
2014-03-26 21:56:02 +0900

    Initial Commit

---
+++ iphone/140326/EmptyTabNaviEx2/EmptyTabNaviEx2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  EmptyTabNaviEx2
+//
+//  Created by 이지 on 14. 3. 26..
+//  Copyright (c) 2014년 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iphone/140326/EmptyTabNaviEx2/EmptyTabNaviEx2Tests/EmptyTabNaviEx2Tests.m
@@ -0,0 +1,34 @@
+//
+//  EmptyTabNaviEx2Tests.m
+//  EmptyTabNaviEx2Tests
+//
+//  Created by 이지 on 14. 3. 26..
+//  Copyright (c) 2014년 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface EmptyTabNaviEx2Tests : XCTestCase
+
+@end
+
+@implementation EmptyTabNaviEx2Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
