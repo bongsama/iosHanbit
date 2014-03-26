6e6dee1c19cf1bdfb8a76f234d0e0203d649fe43
bongsama <kim@bongkwan.com>
2014-03-26 21:12:26 +0900

    autosave

--- iphone/emptyNaviEx1/emptyNaviEx1/FirstViewController.m
+++ iphone/emptyNaviEx1/emptyNaviEx1/FirstViewController.m
@@ -1,17 +1,54 @@
 //
 //  FirstViewController.m
-//  emptyNaviEx1
+//  EmptyNaviEx1
 //
-//  Created by bongkwan on 2014. 3. 26..
-//  Copyright (c) 2014년 저녁반. All rights reserved.
+//  Created by 이지 on 14. 3. 26..
+//  Copyright (c) 2014년 이지. All rights reserved.
 //
 
 #import "FirstViewController.h"
 
+@interface FirstViewController ()
+
+@end
+
 @implementation FirstViewController
 
-- (void)viewDidLoad{
-    [super viewDidLoad];
-  _label.text=@"First Veiw";
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
 }
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    [_label setFont:[UIFont boldSystemFontOfSize:20]];
+    [_label setTextAlignment:NSTextAlignmentCenter];
+    
+    [_label setTextColor:[UIColor blueColor]];
+    [_label setText:@"아무거나"];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+ {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
+
 @end
