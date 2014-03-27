eadce114b07b000c298f545f1c1735575c9dfd1b
bongsama <kim@bongkwan.com>
2014-03-27 19:43:03 +0900

    autosave

--- iphone/EmptyNaviXibEx/EmptyNaviXibEx/AppDelegate.m
+++ iphone/EmptyNaviXibEx/EmptyNaviXibEx/AppDelegate.m
@@ -16,7 +16,7 @@
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
-    _firstView=[[]
+    _firstView=[[FirstViewController alloc]initWithNibName:@"FirstViewController" bundle:nil];
     
     return YES;
 }
