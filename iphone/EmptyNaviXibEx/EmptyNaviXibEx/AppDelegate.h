3b6cab628cc9647782c1dfe0834554f9c9bcf656
bongsama <kim@bongkwan.com>
2014-03-27 19:56:03 +0900

    autosave

--- iphone/EmptyNaviXibEx/EmptyNaviXibEx/AppDelegate.h
+++ iphone/EmptyNaviXibEx/EmptyNaviXibEx/AppDelegate.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 #import "FirstViewController.h"
-#import "FirstViewController.h"
+#import "SecoudViewController.h"
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
