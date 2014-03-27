44589835557d8320d3bb5a26f387343424131099
bongsama <kim@bongkwan.com>
2014-03-27 19:55:42 +0900

    autosave

---
+++ iphone/EmptyNaviXibEx/SecoudViewController.m
@@ -0,0 +1,38 @@
+//
+//  SecoudViewController.m
+//  EmptyNaviXibEx
+//
+//  Created by bongkwan on 2014. 3. 27..
+//  Copyright (c) 2014년 저녁반. All rights reserved.
+//
+
+#import "SecoudViewController.h"
+
+@interface SecoudViewController ()
+
+@end
+
+@implementation SecoudViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
