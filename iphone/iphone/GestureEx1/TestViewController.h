a98689cbb6b1cc3b441335da1faf58c4e0e59324
bongsama <kim@bongkwan.com>
2014-03-31 19:46:33 +0900

    autosave

---
+++ iphone/iphone/GestureEx1/TestViewController.h
@@ -0,0 +1,18 @@
+//
+//  TestViewController.h
+//  GestureEx1
+//
+//  Created by bongkwan on 2014. 3. 31..
+//  Copyright (c) 2014년 저녁반. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "TestViewController.h"
+@interface TestViewController : UIViewController
+
+@property (strong, nonatomic) UIWindow 
+
+
+
+
+@end
